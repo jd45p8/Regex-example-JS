99a019d1be1e4193ed25bd01a37c52de2b0d3a48
Fabian Osorio Iriarte <fabiani@uninorte.edu.co>
2019-11-06 11:56:48 -0500

    Pequeñas correciones

--- scripts/real.js
+++ scripts/real.js
@@ -1,9 +1,9 @@
 /**
  * Checks out whether the given string match a valid real number with the given quantity of decimals
  * @param {String} input input to checkout.
- * @param {number} decimalCount quantity of decimals that should hava a valid real. 
+ * @param {number} decimalCount quantity of decimals that should hava a valid real.
  */
 function isValidReal(input, decimalCount) {
-    const regex = RegExp(`^[+-]?\\d+(\\.\\d{${decimalCount}}){${Math.min(decimalCount, 1)}}$`)
+    const regex = RegExp(`^[+-]?\\d*(\\.\\d{${decimalCount}}){${Math.min(decimalCount, 1)}}$`)
     return regex.test(input)
 }
